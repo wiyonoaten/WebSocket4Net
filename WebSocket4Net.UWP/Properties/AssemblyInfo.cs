f074ef2b5f84fed175ca6f2025b39c7cf982b4a0
Wiyono Aten <wiyono.aten@outlook.com>
2015-09-08 22:50:51 +0100

    Build and packaging fixes for UWP.

---
+++ WebSocket4Net.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebSocket4Net for UWP")]
+[assembly: AssemblyDescription("WebSocket4Net for UWP")]
+[assembly: ComVisible(false)]
+[assembly: Guid("BED5C897-F4BD-462C-A64D-F24AEF185A2C")]
---
+++ package.cmd
@@ -0,0 +1,2 @@
+tools\nuget.exe update -self
+tools\nuget.exe pack WebSocket4Net.nuspec -BasePath NuGet\Package -Output NuGet
