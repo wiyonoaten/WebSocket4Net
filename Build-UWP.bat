5c83a5287f433041a8f449946a494cea56961b38
Wiyono Aten <wiyono.aten@outlook.com>
2015-09-10 21:20:41 +0100

    Build script fixes for UWP.

--- Build-UWP.bat
+++ Build-UWP.bat
@@ -8,10 +8,20 @@ if not exist %fdir% (
 
 set msbuild=%fdir%\msbuild.exe
 
+set outDir=bin\UWP\Debug
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-%msbuild% WebSocket4Net.UWP.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\UWP\Debug
+%msbuild% WebSocket4Net.UWP.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutDir=..\%outDir%
 
+set outDir=%outDir%\WebSocket4Net.UWP
+del %outDir%\*.pdb
+del %outDir%\*.pri
+
+set outDir=bin\UWP\Release
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-%msbuild% WebSocket4Net.UWP.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\UWP\Release
+%msbuild% WebSocket4Net.UWP.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutDir=..\%outDir%
+
+set outDir=%outDir%\WebSocket4Net.UWP
+del %outDir%\*.pdb
+del %outDir%\*.pri
 
 pause
